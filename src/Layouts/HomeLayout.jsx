643f3715e61ca7dd8f96afb818b9c0ff34b831da
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-11-14 00:14:43 +0530

    Marquee Implement done on Latest News

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,9 +1,13 @@
 import Header from "../Components/Header";
+import LatestNews from "../Components/LatestNews";
 
 const HomeLayout = () => {
     return (
         <div>
            <Header></Header>
+           <section className="max-w-5xl mx-auto">
+            <LatestNews></LatestNews>
+           </section>
         </div>
     );
 };
