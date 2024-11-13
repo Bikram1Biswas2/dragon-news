2119732711798e165d72c2d5c2ed2b097cdfd127
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-11-14 01:03:00 +0530

    LeftSide AllCategory fetched and show on ui

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,19 +1,28 @@
 import Header from "../Components/Header";
 import LatestNews from "../Components/LatestNews";
+import LeftSide from "../Components/MainLayOut/LeftSide";
 import Navbar from "../Components/Navbar";
 
 const HomeLayout = () => {
     return (
         <div className="font-poppins space-y-4">
+            {/* header */}
           <header>
           <Header></Header>
-           <section className="max-w-5xl mx-auto">
+           <section className="max-w-6xl mx-auto">
             <LatestNews></LatestNews>
            </section>
           </header>
-          <nav className="max-w-5xl mx-auto">
+          {/* Navbar */}
+          <nav className="max-w-6xl mx-auto">
             <Navbar></Navbar>
           </nav>
+          {/* Main */}
+         <main className="max-w-6xl mx-auto md:grid grid-cols-12 gap-3">
+              <aside className="col-span-3"><LeftSide></LeftSide></aside>
+              <section className="col-span-6">Main Content</section>
+              <aside className="col-span-3">Right Side</aside>
+         </main>
         </div>
     );
 };
